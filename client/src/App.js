fcd4e14eb8f87fd27ea88bb20cd88dad54671622
sumanthmunikoti <sumanthmunikoti1781989@gmail.com>
2020-05-05 20:02:04 -0400

    Halfway through

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,13 @@
 import React from "react"
 import { BrowserRouter as Router, Route } from "react-router-dom"
 
-import Join from './components/Join'
-import Chat from './components/Chat'
+import Join from './components/Join/Join'
+import Chat from './components/Chat/Chat'
 
 const App = () => {
     return (
-        // <h1>This sucks</h1>
         <Router>
-            <Route path="/" component={Join} />
+            <Route path="/" exact component={Join} />
             <Route path="/chat" component={Chat} />
         </Router>
     )
